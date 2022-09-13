a2583b1ffc355b88d72c09cfd40d8d193a5f9dca
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-09-13 18:28:12 -0500

    commit tree 9f97d6fb6ccd6373326e68822a8eba828dbc22f1

--- looping.rb
+++ looping.rb
@@ -8,8 +8,6 @@ def happy_new_year
 end
 
 
-
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -23,9 +21,15 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  
+  reversed_str = ""
+  str.length.times do |i|
+    reversed_str = str[i] + reversed_str
+  end
+  reversed_str
 end
